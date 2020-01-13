6a6bad856f6cd848311ffdbfe65f54c8a650c625
AleksandrKaradzikov <karadzikovaleksandr@gmail.com>
2020-01-13 16:46:59 +0300

    Updates

---
+++ precache-manifest.8b59c262b30edc3801e707f6f8f4bc5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c47336480d7a083df7203304281ceaa",
+    "url": "/blog-platform/index.html"
+  },
+  {
+    "revision": "b4a9dcb75d63f2c5c1ae",
+    "url": "/blog-platform/static/css/2.0f35903b.chunk.css"
+  },
+  {
+    "revision": "11b663daba3162ce8de5",
+    "url": "/blog-platform/static/css/main.119f96ca.chunk.css"
+  },
+  {
+    "revision": "b4a9dcb75d63f2c5c1ae",
+    "url": "/blog-platform/static/js/2.abe50320.chunk.js"
+  },
+  {
+    "revision": "0d4ad65ca5ba2f75ab9abfb8880c5a09",
+    "url": "/blog-platform/static/js/2.abe50320.chunk.js.LICENSE"
+  },
+  {
+    "revision": "11b663daba3162ce8de5",
+    "url": "/blog-platform/static/js/main.d390353d.chunk.js"
+  },
+  {
+    "revision": "0b0d219989c46d97d28b",
+    "url": "/blog-platform/static/js/runtime-main.ec9d9bae.js"
+  }
+]);
